8373c7083554a3fc6214043d80299d481cfb7c08
Punyugi2012 <58160216@go.buu.ac.th>
2019-06-15 15:37:17 +0700

    Initial Commit

---
+++ Wongnai/Controllers/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabbarController.swift
+//  Wongnai
+//
+//  Created by punyawee jaroentao on 15/6/2562 BE.
+//  Copyright © 2562 punyawee jaroentao. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
