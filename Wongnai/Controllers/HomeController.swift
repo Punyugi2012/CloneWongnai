160191b7365cf7d4f5cddd398bdf5ccd461b778b
Punyugi2012 <58160216@go.buu.ac.th>
2019-06-15 16:25:20 +0700

    Setup HomeController

---
+++ Wongnai/Controllers/HomeController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeController.swift
+//  Wongnai
+//
+//  Created by punyawee jaroentao on 15/6/2562 BE.
+//  Copyright © 2562 punyawee jaroentao. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let cellID = "cellID"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView.backgroundColor = .white
+        self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.frame.width, height: 200)
+    }
+    
+}
--- Wongnai/Controllers/MainTabBarController.swift
+++ Wongnai/Controllers/MainTabBarController.swift
@@ -12,6 +12,25 @@ class MainTabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let homeController = HomeController(collectionViewLayout: UICollectionViewFlowLayout())
+        let homeNavigationController = UINavigationController(rootViewController: homeController)
+        homeController.tabBarItem = UITabBarItem(tabBarSystemItem: .mostViewed, tag: 0)
+        
+        let recentReviewController = UIViewController()
+        recentReviewController.tabBarItem = UITabBarItem(tabBarSystemItem: .recents, tag: 0)
+        
+        let cookController = UIViewController()
+        cookController.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 0)
+        
+        let bookmarksController = UIViewController()
+        bookmarksController.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 0)
+        
+        let profileController = UIViewController()
+        profileController.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 0)
+        
+        viewControllers = [homeNavigationController, recentReviewController, cookController, bookmarksController, profileController]
+        
     }
     
 }
